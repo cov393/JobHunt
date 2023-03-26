e1e9c87b502dff13731a44d235fa74bb373a6504
cov393 <covuk393@gmail.com>
2023-03-27 00:04:28 +0100

    Parsing the information between pages

--- src/views/JobDetails.vue
+++ src/views/JobDetails.vue
@@ -1,119 +1,64 @@
-<!-- <template>
-    <div class="container mx-auto p-4">
-      <div class="border-2 border-gray-300 rounded p-4">
-        <h2 class="text-2xl font-semibold mb-2">{{ job.job_title }}</h2>
-
-        <p>P Description: {{description }}</p>
-        <input v-model="searchQuery"/>
-        <div
-            v-for="job in filteredJobs"
-            :key="job.title">
-        </div>
-
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import jobsData from "../database/database.json";
-
-
-  
-  export default {
-    name: "job_details",
-    props:['description'],
-    data() {
-      return {
-        job:'',
-        search: "",
-        searchQuery: "",
-        sortOption: "latest-date",
-        selectedCategory: "all",
-        jobs: jobsData.jobs,
-        sortBy: "category",
-      };
-    },    
-    computed: {
-        categories() {
-      const categoriesSet = new Set();
-      for (const job of this.jobs) {
-        categoriesSet.add(job.category);
-      }
-      return Array.from(categoriesSet);
-    },
-      filteredJobs() {
-        let jobs = this.jobs.filter((job) => {
-        const titleMatch = job.job_title
-          .toLowerCase()
-          .includes(this.searchQuery.toLowerCase());
-        const categoryMatch =
-          this.selectedCategory === "all" ||
-          job.category === this.selectedCategory;
-        return titleMatch && categoryMatch;})
-      },
-    },
-    watch: {
-        title() {
-            console.log('props details');
-            this.getPostDetails();
-        },
-    },  
-    created() {
-      
-        console.log(this.$route.params);
-        //this.getPostDetails();
-    },
-
-  };
-  </script>
-   -->
-
-   <template>
+<template>
   <div class="container mx-auto p-4">
     <div class="border-2 border-gray-300 rounded p-4">
+      <router-link :to="{ name: 'job_search' }" class="btn btn-primary">
+        <button>back</button>
+      </router-link>
       <h2 class="text-2xl font-semibold mb-2">{{ title }}</h2>
-      <p>Job Description: {{ description }}</p>
-      <p>Job Category: {{ category }}</p>
-      <p>Job Type: {{ job.job_type }}</p>
-      <p>Job Location: {{ job.location }}</p>
-      <!-- Add any other job details you want to display here -->
+      <div v-for="(item, itemIndex) in find_data" :key="itemIndex">
+        <p>Job ID</p>
+        <p>{{ item.location }}</p>
+        <p>£{{ item.salary_from }}</p>
+      </div>
+      <p>
+         Posted {{ passDaysCount }} days ago
+      </p>
     </div>
   </div>
 </template>
   
   <script>
 import jobsData from "../database/database.json";
+import JobItem from "./JobItem.vue";
 
 export default {
+  components: {
+    JobItem,
+  },
   name: "job_details",
-  props: ["description","title","category"],
-  
+  props: ["description", "title", "category"],
+
   data() {
     return {
-      job: {
-        job_title: "",
-        description: "",
-        category: "",
-        job_type: "",
-        location: "",
-        // Add any other job properties with default values here
-      },
-      selectedCategory: "all",
+      DataList: [],
+      value: undefined,
       jobs: jobsData.jobs,
+      days_pass: null,
     };
-  },  
-
-  computed: {
-    categories() {
-      const categoriesSet = new Set();
-      for (const job of this.jobs) {
-        categoriesSet.add(job.category);
-      }
-      return Array.from(categoriesSet);
-    },
   },
-  created() {
-    console.log(this.$route.params);
+  computed: {
+    // DATE CALCULATION
+    passDaysCount() {
+      const today_date = new Date();
+      const pass_date = new Date(this.DataList[0][0].posted_date);
+      const days = (today_date, pass_date) => {
+        let diff = today_date.getTime() - pass_date.getTime();
+        let totalDays = Math.ceil(diff / (1000 * 3600 * 24));
+        return totalDays;
+      };
+      return days(today_date, pass_date);
+    },
+
+    // PASSED DATABASE FILTER
+    find_data() {
+      const dataB = this.jobs.filter((par) => {
+        return par.job_description == this.description;
+      });
+      this.DataList.push(dataB);
+      this.value = undefined;
+      console.log(this.DataList);
+      return dataB;
+    },
   },
 };
 </script>
